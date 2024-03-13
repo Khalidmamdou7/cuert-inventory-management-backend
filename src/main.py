6f414e991cce1e16140e28f1e84bbf670967da90
Khaled Mamdouh <khalidmamdou7@gmail.com>
2024-03-14 00:48:44 +0200

    Allow CORS from everywhere

--- src/main.py
+++ src/main.py
@@ -49,7 +49,8 @@ origins = [
     "http://localhost",
     "http://localhost:8080",
     "http://localhost:5173",
-    "http://localhost:3000"
+    "http://localhost:3000",
+    "*"
 ]
 
 app.add_middleware(
